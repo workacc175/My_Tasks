ec0a71eab90ff9b118fb16b19b7e5da636350ce9
workacc175 <work.acc175@gmail.com>
2026-01-28 03:36:56 +0200

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,19 @@
+Item_1 = 5
+Item_2 = 18
+Item_3 = 27
+
+total_budget = 36.5
+
+total_cost = Item_1 + Item_2 + Item_3
+
+Exchange = total_budget - total_cost
+
+more_money_needed = False
+
+print("Chips:" , "$" , Item_1)
+print("chocolate:" , "$" , Item_2)
+print("large Soda:" , "$" , Item_3)
+print("Total:" , "$" , total_cost)
+print("cash:" , "$" , total_budget)
+print("change:" , "$" , Exchange)
+print("Sufficnet Funds?:", more_money_needed, ",Not Enough Cash")
